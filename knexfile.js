7af0e68b49757de5b19bd75c822d1b32163354ba
patpalmerston <patpalmerston@gmail.com>
2019-05-24 08:59:51 -0700

    testing table set up and created

--- knexfile.js
+++ knexfile.js
@@ -20,4 +20,17 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
